067193c4092e9600997b0dc11b0ca92a0b5f04e0
Mark Ditsworth <markditsworth@protonmail.com>
2020-03-23 15:45:09 -0500

    got elastic and kibana working on docker network

--- elasticsearch/run.sh
+++ elasticsearch/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -d -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" -v $(pwd)/elasticsearch.yml:/usr/share/elasticsearch/config/elasticsearch.yml --name elasticsearch elasticsearch
+docker run -d -p 9200:9200 -p 9300:9300 --network kafka-network -e "discovery.type=single-node" -v $(pwd)/elasticsearch.yml:/usr/share/elasticsearch/config/elasticsearch.yml --name elasticsearch elasticsearch
