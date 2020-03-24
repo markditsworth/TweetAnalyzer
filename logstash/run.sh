185178ca3194b38099d91d8bddf15bb69ba4a813
Mark Ditsworth <markditsworth@protonmail.com>
2020-03-24 11:01:22 -0500

    add -d to docker run for logstash

--- logstash/run.sh
+++ logstash/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#Todo: add -d
-docker run -it --name logstash --network kafka-network -v $(pwd)/logstash.yml:/usr/share/logstash/config/logstash.yml -v $(pwd)/pipeline.conf:/usr/share/logstash/pipeline/logstash.conf logstash
+
+docker run -itd --name logstash --network kafka-network -v $(pwd)/logstash.yml:/usr/share/logstash/config/logstash.yml -v $(pwd)/pipeline.conf:/usr/share/logstash/pipeline/logstash.conf logstash
