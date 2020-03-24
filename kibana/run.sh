055fa4a1cdf0017a39b9f17859cf0d1e0ecbdc6f
Mark Ditsworth <markditsworth@protonmail.com>
2020-03-24 11:35:00 -0500

    add -d in kibana docker run

--- kibana/run.sh
+++ kibana/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --name kibana --network kafka-network -v $(pwd)/kibana.yml:/usr/share/kibana/config/kibana.yml -p 5601:5601 kibana
+docker run -d --name kibana --network kafka-network -v $(pwd)/kibana.yml:/usr/share/kibana/config/kibana.yml -p 5601:5601 kibana
